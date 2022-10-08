dcc241524f380c9ce4d65f82cb7bef7726360246
jesteen1 <108133502+jesteen1@users.noreply.github.com>
2022-10-08 09:57:54 +0530

    Add files via upload

---
+++ school design/loader.js
@@ -0,0 +1,18 @@
+
+function loadImage(){
+    var image=document.querySelectorAll('#blur')
+
+    for(var i=0 ;i<image.length;i++){
+        image[i].classList.remove('blur');
+    }
+
+}
+
+function loadImage2(){
+    var image=document.querySelectorAll('#blur1')
+
+    for(var i=0 ;i<image.length;i++){
+        image[i].classList.remove('blur1');
+    }
+}
+
