46e1bc51d481b59917a8a63780260e1441111aca
jesteen1 <108133502+jesteen1@users.noreply.github.com>
2022-10-16 19:44:11 +0530

    Add files via upload

---
+++ loader.js
@@ -0,0 +1,21 @@
+
+function loadImage(){
+    var image=document.querySelectorAll('#blur')
+
+    for(var i=0 ;i<image.length;i++){
+        image[i].classList.remove('blur');
+    }
+
+}
+
+function loadImage2(){
+   
+var conent_image=document.querySelectorAll('#blur1')
+    for(var i=0 ;i<image.length;i++){
+        image[i].classList.remove('blur1');
+    }
+    for(var i=0 ;i<image.length;i++){
+        image[i].classList.remove('blur1');
+    }
+}
+
